80e812e4640e715a5d80c5d094ef6db0c1cf2921
Flex Alderson <phantoinang2001@gmail.com>
2021-09-13 22:44:29 +0700

    try gain

--- Chapter02/FirstConstract.sol
+++ Chapter02/FirstConstract.sol
@@ -1,10 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.7.0 <0.9.0;
 contract FirstConstract {
-    enum State { Created, Locked, Inactive } 
-    State public state;
-
-    function isLocked() public view returns(bool){
-        return state == State.Locked;
-    }
+    
 }
